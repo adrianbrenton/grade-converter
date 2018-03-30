f342de9fd179fa99f4071760b6960c4a2db2c405
adrianbrenton <adrian.r.brenton@gmail.com>
2018-03-30 23:51:49 +0100

    Add a basic first draft for the GUI

---
+++ view/main_view.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+root = Tk()
+
+
+welcomeMessage = Label(root, text="Welcome to the Grade Adjuster!")
+openFileButton = Button(root, text="Open File")
+openFolderButton = Button(root, text="Open Folder")
+outputDirectoryButton = Button(root, text="Choose Output Folder")
+outputDirectoryLabel = Label(root)
+convertButton = Button(root, text="Convert!")
+
+
+welcomeMessage.pack(side=BOTTOM)
+openFileButton.pack()
+openFolderButton.pack()
+outputDirectoryButton.pack()
+outputDirectoryLabel.pack()
+convertButton.pack()
+
+
