72e1b70a5c67fae20a1b9071c391971b6199f5e2
adrianbrenton <adrian.r.brenton@gmail.com>
2018-03-30 23:55:41 +0100

    Add the controller file for the converter

---
+++ controller/main_controller.py
@@ -0,0 +1,34 @@
+from view import main_view
+from model import main_model
+from tkinter import filedialog
+
+
+def run_open_file_dialog():
+    filename = filedialog.askopenfilename(initialdir="~", title="Select file",
+                                          filetypes=(("csv files", "*.csv"),))
+    if filename:
+        main_model.selectedFiles.add(filename)
+
+
+def run_open_folder_dialog():
+    folder_name = filedialog.askdirectory(initialdir="~",
+                                          title="Select folder")
+    if folder_name:
+        main_model.selectedDirectories.add(folder_name)
+
+
+def select_output_directory():
+    folder_name = filedialog.askdirectory(initialdir="~",
+                                          title="Select output folder")
+    if folder_name:
+        main_model.outputDirectory = folder_name
+        main_view.outputDirectoryLabel.config(text="Output to: {}".format(
+            folder_name))
+
+
+main_view.openFileButton.config(command=run_open_file_dialog)
+main_view.openFolderButton.config(command=run_open_folder_dialog)
+main_view.outputDirectoryButton.config(command=select_output_directory)
+main_view.convertButton.config(command=main_model.convert_selected_files)
+
+main_view.root.mainloop()
