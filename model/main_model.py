ac690763cb60eac17de330be335c4f3556e94c43
adrianbrenton <adrian.r.brenton@gmail.com>
2018-03-30 23:53:37 +0100

    Add the model files for the converter

---
+++ model/main_model.py
@@ -0,0 +1,27 @@
+from glob import glob
+
+from model import test_grade_converter as converter
+
+
+def files_in_directories(directory_set):
+    """Return a set of file paths from a set of directories"""
+    csv_set = set()
+    for directory in directory_set:
+        for file_path in glob(directory + '/*'):
+            if file_path.endswith('.csv'):
+                csv_set.add(file_path)
+    # TODO: consider set comprehension above instead of nested for-loops.
+    return csv_set
+
+
+def convert_selected_files():
+    """Calls operate_on_csv method of test_grade_converter on selected files"""
+    file_set = selectedFiles.union(files_in_directories(selectedDirectories))
+    for filename in file_set:
+        converter.operate_on_csv(filename, output_dir=outputDirectory)
+
+
+selectedFiles = set()
+selectedDirectories = set()
+outputDirectory = ''
+
---
+++ model/test_grade_converter.py
@@ -0,0 +1,70 @@
+"""Adjust grade spreadsheets based on new grade boundaries"""
+
+# TODO Create Docstrings
+# TODO Add this module to the model directory (because this is part of the business logic)
+# TODO (create model, view and controller directories)
+# TODO: change all output directories to be specified by user
+
+import csv
+from math import ceil
+from os.path import splitext
+import xlsxwriter
+
+
+def percent_to_float(percentage):
+    """Return a float by stripping whitespace and percent symbol"""
+    return float(str(percentage).strip().rstrip('%').rstrip())
+
+
+def converted_score(score):
+    """Return the adjusted score as a float, calculated from score argument"""
+    # level = [i for i, boundary in enumerate]
+    for i, boundary in enumerate(gradeBoundaries):
+        if boundary > score:
+            level = i
+            break
+    else:
+        level = 5
+    new_score = ceil(
+        (40 + level * 10 + 10 * (score - gradeBoundaries[level - 1]) /
+         (gradeBoundaries[level] - gradeBoundaries[level - 1])) * 100) / 100
+    return new_score
+
+
+def operate_on_line(line, adjuster=converted_score):
+    """Return a line from the csv with the student's score converted"""
+    score = percent_to_float(line[2])
+    new_line = line[:2]
+    new_line.append(adjuster(score))  # TODO: adjuster works?
+    return new_line
+
+
+def operate_on_csv(csv_path, output_dir=''):
+    """Opens the given csv file and calls operate_on_line() on each line"""
+    with open(csv_path, 'r') as scoresFile:
+        reader = csv.reader(scoresFile)
+        output_file_name = splitext(scoresFile.name)[0].split('/')[
+                               -1] + '_adjusted'
+        list_of_rows = list(reader)[2:]
+
+    new_rows_list = [operate_on_line(line) for i, line in enumerate(
+        list_of_rows[1:])]  # TODO: is this enumerate() necessary?
+
+    workbook = xlsxwriter.Workbook(
+        output_dir + '/' + output_file_name + '.xlsx')
+    worksheet = workbook.add_worksheet()
+    worksheet.set_column(0, 0, 12)
+    worksheet.set_column(1, 1, 25)
+    worksheet.set_column(2, 2, 14)
+
+    bold_format = workbook.add_format({'bold': True})
+    worksheet.write_row('A1', ('Student ID', 'Student Name', 'Adjusted Score'),
+                        bold_format)
+
+    for i, line in enumerate(new_rows_list):
+        worksheet.write_row(i + 1, 0, line)
+
+    workbook.close()
+
+
+gradeBoundaries = [0, 41, 48, 62, 71, 100]
